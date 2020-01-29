a2e0dcb6ddc8234da08cc42d781f9c4cd167b4ee
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2020-01-29 16:30:57 +0200

    Updates

---
+++ precache-manifest.08bb9e9d604c09ee881921c09b183821.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df13a8520c8a389f6971e7b2e3bc7067",
+    "url": "/food_delivery/index.html"
+  },
+  {
+    "revision": "c18cceb0533dfe5748fa",
+    "url": "/food_delivery/static/css/2.6baff533.chunk.css"
+  },
+  {
+    "revision": "78f701bd93167fd09573",
+    "url": "/food_delivery/static/css/main.ebbbce68.chunk.css"
+  },
+  {
+    "revision": "c18cceb0533dfe5748fa",
+    "url": "/food_delivery/static/js/2.046b3e04.chunk.js"
+  },
+  {
+    "revision": "78f701bd93167fd09573",
+    "url": "/food_delivery/static/js/main.f25771ca.chunk.js"
+  },
+  {
+    "revision": "e00085530f8c774170cc",
+    "url": "/food_delivery/static/js/runtime-main.088a6087.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/food_delivery/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/food_delivery/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/food_delivery/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/food_delivery/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/food_delivery/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
